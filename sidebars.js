73c1888d5da35273343adaa99a48865967f013d4
John IV <john.iv@x25bd.com>
2021-02-22 14:52:52 +0000

    Testing Docusaurus - some OS Data Hub documentation configured

---
+++ sidebars.js
@@ -0,0 +1,42 @@
+module.exports = {
+  docs: {
+    "OS Data Hub": [
+      'os-data-hub/quickstart', 
+      'os-data-hub/overview', 
+      'os-data-hub/faq'
+    ],
+    "OS Maps API": [
+      'os-maps-api/quickstart', 
+      'os-maps-api/overview', 
+      'os-maps-api/technical-specification'
+    ],
+    "OS Vector Tile API": [
+      'os-vector-tile-api/quickstart', 
+      'os-vector-tile-api/overview', 
+      'os-vector-tile-api/technical-specification'
+    ],
+    "OS Features API": [
+      'os-features-api/quickstart', 
+      'os-features-api/overview', 
+      'os-features-api/technical-specification'
+    ],
+    "OS Linked Identifiers API": [
+      'os-linked-identifiers-api/quickstart', 
+      'os-linked-identifiers-api/overview', 
+      'os-linked-identifiers-api/technical-specification'
+    ],
+    "OS Places API": [
+      'os-places-api/quickstart', 
+      'os-places-api/overview', 
+      'os-places-api/technical-specification'
+    ],
+    Docusaurus: [
+      'welcome', 
+      'doc2', 
+      'doc3'
+    ],
+    Features: [
+      'mdx'
+    ],
+  },
+};
